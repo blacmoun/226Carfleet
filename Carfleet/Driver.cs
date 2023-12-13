06315cf438c1248cf52b6efc0a02be5645438e2d
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-12-13 10:14:04 +0100

    feat: all tests successful

--- Carfleet/Driver.cs
+++ Carfleet/Driver.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Carfleet
 {
@@ -6,6 +8,7 @@ namespace Carfleet
     {
         #region private attributes
         private string _workZone;
+        private Vehicle _vehicle = null;
         #endregion private attributes
 
         #region public methods
@@ -14,6 +17,37 @@ namespace Carfleet
             WorkZone = workZone;
         }
         public string WorkZone { get => _workZone; set => _workZone = value; }
+        public Vehicle Vehicle
+        {
+            get
+            {
+                return _vehicle;
+            }
+        }
+
+        public void TakeAVehicle(Vehicle vehicle)
+        {
+            if(_vehicle != null)
+            {
+                throw new VehicleAlreadyAssignedException();
+            }
+            _vehicle = vehicle;
+        }
+
+        public void ReleaseAVehicle()
+        {
+            if (_vehicle == null)
+            {
+                throw new NoVehicleAssignedException();
+            }
+            _vehicle = null;
+        }
         #endregion public methods
+
+        #region embedded classes
+        public class DriverException : Exception { }
+        public class VehicleAlreadyAssignedException : DriverException { }
+        public class NoVehicleAssignedException : DriverException { }
+        #endregion embedded classes
     }
 }
